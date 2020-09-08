57bc8a7f2e56da8c71644df16d63f3ca30509d1f
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2020-09-07 22:32:04 -0500

    delete false assertion

--- spec/green_garden_spec.rb
+++ spec/green_garden_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe GreenGarden do
   it "has a version number" do
     expect(GreenGarden::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
