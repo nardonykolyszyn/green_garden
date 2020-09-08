894a4a4ffe8442c18e37a016c9d6ba473c43fb4e
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2020-09-08 02:50:56 -0500

    assert positive and false datasets comparasion

--- spec/green_garden_spec.rb
+++ spec/green_garden_spec.rb
@@ -8,13 +8,27 @@ RSpec.describe GreenGarden do
   end
 
   describe '::Tree' do
-    let(:graph_one) { GreenGarden::Tree.load_json(load_fixture('BreweriesMaster.json')) }
-    let(:graph_two) { GreenGarden::Tree.load_json(load_fixture('BreweriesSample1.json')) }
+    let(:graph_one) { load_fixture('BreweriesMaster.json') }
+    let(:graph_two) { load_fixture('BreweriesSample1.json') }
+    let(:graph_three) { load_fixture('BreweriesSample4.json') }
 
     let(:invalid_dataset_path) { Pathname.new('invalid/dataset.json').to_s }
+
     context 'when invalid datasets are passed' do
       it 'shows a custom error message when files do not exist' do
         expect { GreenGarden::Tree.diff_between(invalid_dataset_path, invalid_dataset_path) }.to raise_error(GreenGarden::Tree::DatasetNotFound)
+      end
+    end
+
+    context 'when datasets are equal' do
+      it 'returns true as success response' do
+        expect(GreenGarden::Tree.diff_between(graph_one, graph_two)).to be true
+      end
+    end
+
+    context 'when datasets are different' do
+      it 'returns false as a failure response' do
+        expect(GreenGarden::Tree.diff_between(graph_one, graph_three)).to be false
       end
     end
   end
