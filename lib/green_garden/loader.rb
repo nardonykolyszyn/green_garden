3fab284c24cc2876c7e0a5420cdf37aef57dbff2
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2020-09-07 22:12:37 -0500

    development/create_loader_module

---
+++ lib/green_garden/loader.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'oj'
+
+# This module provides a fast and non-disruptive way to load heavy JSON files
+# Oj helps to carry on with this task by providing an optimized parsing and marshalling interface
+module Loader
+  def load_json(file_path, read_mode = 'r')
+    File.open(file_path, read_mode) { |f| Oj.load(f.read) }
+  end
+end
